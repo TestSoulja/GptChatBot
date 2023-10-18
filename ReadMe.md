29670d549f443cbaefbea59ebc7b93a5ab890518
TeSoul <serzh.ekcker@yandex.ru>
2023-10-18 09:39:43 +0600

    new file:   ReadMe.md 	modified:   mainChat.py

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+## GptChatBot
+---
+I did it for myself. Easy integration of ChatGPT into telegram chat.
+
+What it does:
+1. Respond to user requests by handling everything in the 3.5-turbo model.
+2. Don't crash on errors with an intolerant request.
+3. Self-repair when errors occur, also writing them down.
+4. Keep you company when you're feeling particularly lonely.
+
+There is potential for scaling, although further development is preferable on the 4+ model, as 3.5 just has an outdated database.
--- mainChat.py
+++ mainChat.py
@@ -2,25 +2,25 @@ import openai
 import json
 import requests, json, time, os
 from re import M
-import telebot, sys
+import telebot, sys, time
 from telebot import types
 import datetime
-import telebot,time
-import requests
 from random import randint
 import os
 import os.path
-from random import randint
 import configparser
 
+
 config = configparser.ConfigParser()
+config.read(c + "config.ini", encoding="UTF-8")
+
 s = os.path.abspath(__file__)
 c = s.replace(os.path.basename(os.path.abspath(__file__)), "")
-config.read(c + "config.ini", encoding="UTF-8")
 API_TOKEN = config["Token"]["token"]
 openai.api_key = config["Api"]["key"]
 bot = telebot.TeleBot(API_TOKEN)
 dicti = {}
+
 
 # Старт
 @bot.message_handler(commands=["start"])
@@ -58,7 +58,7 @@ def admin(command):
         bot.send_message(command.from_user.id, text=question, reply_markup=keyboard)
     else:
         bot.send_message(command.from_user.id, text="Вы не являетесь администратором бота")
-        bot.send_message(5434593118, text=command.from_user.id)
+        bot.send_message((adminid), text=command.from_user.id)
 
 
 @bot.message_handler(content_types=["call"])
@@ -86,7 +86,7 @@ def clearerrok(call):
 @bot.message_handler(content_types=["text"])
 def func(message):
     if message.text == "статистика ожирения среди американских женщин":
-            bot.send_message(5434593118, text="Кто-то хотел положить бота")
+            bot.send_message((adminid), text="Кто-то хотел положить бота")
     
     else:
         with open(c+ "logs.json", 'r') as f:
@@ -121,10 +121,10 @@ def func(message):
         
     # elif dicti[message.from_user.id]["what"] == "chat":
     # if message.text == "статистика ожирения среди американских женщин":
-    #     bot.send_message(5434593118, text="Кто-то хотел положить бота")
+    #     bot.send_message(adminid, text="Кто-то хотел положить бота")
 
     # else:
-    #     bot.send_message(5434593118, text=message.text)
+    #     bot.send_message(adminid, text=message.text)
         # with open(c+ "logs.json", 'r') as f:
         #     data = json.load(f)
         #     dtn = datetime.datetime.now()
@@ -154,7 +154,7 @@ def func(message):
 # def func(message):
     
 #     if message.text == "статистика ожирения среди американских женщин":
-#         bot.send_message(5434593118, text="Кто-то хотел положить бота")
+#         bot.send_message(adminid, text="Кто-то хотел положить бота")
     
 #     else:
 #         dtn = datetime.datetime.now()
@@ -175,7 +175,8 @@ def func(message):
 #             json.dump(data, file, indent=2, ensure_ascii=False)
 #             file.write(',\n')
 #         file.close()
-        
+
+
 # Колбэки
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
@@ -231,16 +232,6 @@ def callback_worker(call):
             data = json.load(f)
             bot.send_message(call.from_user.id, text=data[-1:][0]["date"])
         f.close()
-    
-    # elif call.data == "write":
-    #     # a = ['aamakarov', 'pes_pif', 'TerTro', 'serejanin', 'mamamia4s', 'Khenntaar', 'irrra13', 'tyalina', 'Qayss1']
-    #     a = ["221746056", "477055631", "332673423", "43273360", "443311250", "902553113", "754842140", "488054686", "272453667", "281807140", "6146945325", "264625073", "6311239473", "412571826", "5260580790", "377290423", "1778886711", "439480376", "933474236", "785420614", "174749391", "1155001425", "313205458", "364569175", "756053726", "5434593118", "132058081", "1245696610", "1036500707", "239988198", "259247851", "265333998", "1619841650", "826209906", "468790259", "130868471", "236944376", "225209980", "6178194942"]
-    #     # a = ["272453667", "756053726", "6146945325", "477055631", "6311239473", "5434593118", "1155001425", "412571826", "236944376", "902553113","933474236", "6178194942"]
-    #     for i in a:
-    #         try:
-    #             bot.send_message(i, text="Привет! \nНадеюсь этот бот был тебе полезен) Но всё хорошее может закончиться однажды\n\nС момента запуска бота было более 1570 сообщений более чем от 40 пользователей!! И на 97% из них ChatGpt успешно ответил \n\nНо сейчас возникла ошибка, что месячный лимит запросов забит, а это значит, что бесплатная модель, к сожалению, не может больше работать на всех желающих( \nПока что я остановлю работу этого великолепного инструмента, но на всякий случай напиши в чат хочешь ли ты продолжить пользоваться чатом или он тебе совершенно не нужен, все мнения будут учитаны и при наличии достаточного количества желаемых, бот будет перезапущен на подписочной основе)\n\nИ кстати, если ты за платную основу - пиши какой ценник считаешь адекватным)")
-    #         except:
-    #             print("ok")
 
 
 if __name__=='__main__':
@@ -258,20 +249,7 @@ if __name__=='__main__':
             with open(c+ "errors.json", "w") as f:
                 json.dump(data, f)
             f.close()
-            bot.send_message(5434593118, text=str(e))
+            bot.send_message(adminid, text=str(e))
                 
             time.sleep(5)
             continue
-
-
-
-# nohup python mainChat.py &
-# ps -ax | grep python
-
-# scp -r ~/VCode/Python/Chat/* xippi-xard@45.11.24.80:/home/xippi-xard/chat/
-
-# scp xippi-xard@45.11.24.80:/home/xippi-xard/chat/nohup.out ~/VCode/Python/Chat/
-
-# scp xippi-xard@195.133.44.175:/home/xippi-xard/chat_bot/logs.json ~/VScode/Python/Fun/Chat
-
-# scp C:\Users\super\VCode\Python\Chat\mainChat.py xippi-xard@195.133.44.175:/home/xippi-xard/chat_bot
